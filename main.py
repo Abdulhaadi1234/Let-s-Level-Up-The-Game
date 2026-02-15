064b737bf14ad1a9f3e9df2cb8054b9708194875
Abdulhaadi1234 <abdulhaadiahmad21@gmail.com>
2026-02-15 13:14:32 +0500

    Let's Level Up The Game

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+import random
+SCREEN_WIDTH, SCREEN_HEIGHT = 500, 400
+MOVEMENT_SPEED = 5
+FONT_SIZE = 72
+pygame.init()
+font = pygame.font.SysFont("Times New Roman", FONT_SIZE)
+class Sprite(pygame.sprite.Sprite)
+    def __init__(self, color, height, width):
+        super().__init__()
+        self.image = pygame.Surface({width, height})
+        self.image.fill(
+            pygame.color('dodgerblue'))
+        pygame.draw.rect(self.image, color, pygame.Rect(0, 0, width, height))
+        self.rect = self.image.get_rect()
+    def move(self, x_change, y_change):
+        self.rect.x = max(
+            min(self.rect.x + x_change, SCREEN_WIDTH - self.rect.width), 0) 
+        self.rect.y = max(    
+            min(self.rect.y + y_change, SCREEN_HEIGHT - self.rect.height), 0)   
